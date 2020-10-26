df435f299dd671797718652a06b654e7cb6b250b
Cédric JUNG <cedced19@gmail.com>
2020-10-26 23:32:51 +0100

    Add website button

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import HomeScreen from "./app/screens/home.js";
-import ShopsScreen from "./app/screens/shops.js";
+import HomeScreen from './app/screens/home.js';
+import ShopsScreen from './app/screens/shops.js';
+import { Linking } from 'react-native';
 
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Site internet" onPress={() => { Linking.openURL('https://www.decouverto.fr');}} />
+    </DrawerContentScrollView>
+  );
+}
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Accueil" drawerContent={props => <CustomDrawerContent {...props} />}>
         <Drawer.Screen name="Accueil" component={HomeScreen} />
         <Drawer.Screen name="Liste des points de vente" component={ShopsScreen} />
       </Drawer.Navigator>
